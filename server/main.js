de11379bd0e7e8782651d0338efa2e30fac68055
Matteo De Micheli <matteodem@protonmail.com>
2017-04-13 19:26:13 +0200

    add code for sub alien

--- server/main.js
+++ server/main.js
@@ -3,20 +3,56 @@ import {
   generateTypeDefsAndResolvers,
 } from 'meteor/easy:graphqlizer'
 import { createApolloServer } from 'meteor/apollo'
+import { Mongo } from 'meteor/mongo'
 import { makeExecutableSchema } from 'graphql-tools'
 import {
   collection,
   simpleSchema,
 } from './AlienCollection'
 
+const subAlienCollection = new Mongo.Collection('sub-alien')
+const subAlienSimpleSchema = new SimpleSchema({
+  name: {
+    type: String,
+  },
+  bossAlienId: {
+    type: String,
+  },
+})
+
+const subAlienSchema = createCollectionSchema({
+  type: 'SubAlien',
+  collection: subAlienCollection,
+  schema: subAlienSimpleSchema,
+  fields: {
+    type: {
+      bossAlienId: false,
+      bossAlien: {
+        type: 'Alien',
+        resolve: root => collection.findOne({ _id: root.bossAlienId })
+      }
+    }
+  }
+})
+
 const alienSchema = createCollectionSchema({
   type: 'Alien',
   collection: collection,
   schema: simpleSchema,
+  fields: {
+    type: {
+      subAliens: {
+        type: '[SubAlien]',
+        resolve: root => subAlienCollection
+          .find({ bossAlienId: root._id })
+          .fetch(),
+      },
+    },
+  },
 })
 
 const { typeDefs, resolvers } = generateTypeDefsAndResolvers({
-  schemas: [alienSchema],
+  schemas: [subAlienSchema, alienSchema],
 })
 
 const schema = makeExecutableSchema({
